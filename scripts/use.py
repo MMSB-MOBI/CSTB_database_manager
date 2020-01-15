e7c15e5d646be1cdc8cb4716f94785436291f9b2
Cecile Hilpert <cecile.hilpert@gmail.com>
2020-01-15 15:56:53 +0100

    normally all cases are handled

--- scripts/use.py
+++ scripts/use.py
@@ -20,14 +20,68 @@ if __name__ == "__main__":
     db = dbManager.databaseManager(ARGS["--config"])
 
     # Add completely new genome with taxid
-    #db.addGenome('../data_tests/vibrio_cholerae_10k.fna', "Vibrio cholerae O1 biovar El Tor str. N16961", 243277)
+    print("# ADD NEW V.CHOLERAE")
+    db.addGenome('../data_tests/vibrio_cholerae_10k.fna', "Vibrio cholerae O1 biovar El Tor str. N16961", 243277)
+    print()
 
     #Add same genome
-    #db.addGenome('../data_tests/vibrio_cholerae_10k.fna', "Vibrio cholerae O1 biovar El Tor str. N16961", 243277)
+    print("# ADD SAME V.CHOLERAE")
+    db.addGenome('../data_tests/vibrio_cholerae_10k.fna', "Vibrio cholerae O1 biovar El Tor str. N16961", 243277)
+    print()
 
     #Add an other
+    print("# ADD E.COLI")
     db.addGenome("../data_tests/ecoli_O157_1_10k.fna", "Escherichia coli O157:H7 str. EDL933", 155864)
+    print()
 
     #Add other version
+    print("# ADD OTHER VERSION OF E.COLI")
     db.addGenome("../data_tests/ecoli_O157_2_10k.fna", "Escherichia coli O157:H7 str. EDL933", 155864)
+    print()
 
+    #Reinsert old version
+    print("# REINSERT FIRST ECOLI")
+    db.addGenome("../data_tests/ecoli_O157_1_10k.fna", "Escherichia coli O157:H7 str. EDL933", 155864)
+    print()
+
+    #Insert already existed genome with other taxon name
+    print("# INSERT ECOLI WITH OTHER NAME")
+    db.addGenome("../data_tests/ecoli_O157_1_10k.fna", "Pouet", 155864)
+    print()
+
+    #Insert already existed genome with other taxid
+    print("# INSERT ECOLI WITH OTHER TAXID")
+    db.addGenome("../data_tests/ecoli_O157_1_10k.fna", "Escherichia coli O157:H7 str. EDL933", 2)
+    print()
+
+    #Insert already existed genome with other name and other taxid
+    print("# INSERT ECOLI WITH OTHER NAME AND OTHER TAXID")
+    db.addGenome("../data_tests/ecoli_O157_1_10k.fna", "Pouet", 5874)
+    print()
+
+    #Insert already existed genome with taxid that exists 
+    print("# INSERT ECOLI WITH V.CHOLERAE TAXON")
+    db.addGenome('../data_tests/ecoli_O157_1_10k.fna', "Vibrio cholerae O1 biovar El Tor str. N16961", 243277)
+    print()
+
+    print("POUEEEET")
+
+    #Insert plasmid with no taxid
+    print("# INSERT PLASMID WITH NO TAXID")
+    db.addGenome("../data_tests/pOXA-48.fna", "pOXA-48")
+    print()
+
+    #Insert same
+    print("# INSERT SAME PLASMID")
+    db.addGenome("../data_tests/pOXA-48.fna", "pOXA-48")
+    print()
+
+    #Insert under an other name
+    print("# INSERT PLASMID WITH AN OTHER NAME")
+    db.addGenome("../data_tests/pOXA-48.fna", "pOXA")
+    print()
+
+    #Insert suddenly with taxid 
+    print("# INSERT PLASMID SUDDENLY WITH TAXID")
+    db.addGenome("../data_tests/pOXA-48.fna", "pOXA-48", 12)
+    print()
